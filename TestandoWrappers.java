f759d87fe933557b07f04872e2578913e6b8e13a
Vitor Marcato <vitor.marcatof@gmail.com>
2022-09-15 14:35:15 -0300

    testando métodos de um Integer

--- TestandoWrappers.java
+++ TestandoWrappers.java
@@ -19,9 +19,21 @@ public class TestandoWrappers {
         int i1 = lista.get(0);   //tipo primitivo
         Integer i2 = lista.get(1);   //String/objeto
 
+        
+        Integer valorRef = Integer.valueOf(34); //método para atribuir o valor a referencia do tipo Integer 
+        int valorPrimitivo = valorRef.intValue();
+        
+        
         System.out.println(i1);  
         System.out.println(i2); 
+        System.out.println(valorPrimitivo);//Imprime 34.
+        
+        System.out.println(Integer.MAX_VALUE); // valor máximo de um Integer
+        System.out.println(Integer.MIN_VALUE); //valor mínimo de um Integer
 
+        System.out.println(Integer.SIZE); // 32 bits
+        System.out.println(Integer.BYTES); //4 Bytes
+        
 	}
 
 }
